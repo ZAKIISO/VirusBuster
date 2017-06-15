5d21c3ec39ed9ef20028d8a866b6d66ef64d268b
Masanori Ichihashi <nkc.masanori.ichihashi@gmail.com>
2017-06-15 14:55:08 +0900

    触るな危険！

---
+++ Assets/IchihashiFile/EnemyCore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCore : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //当たり判定があったとき
+    void OnTriggerEnter(Collider other)
+    {
+        //交差点
+        if (other.gameObject.tag == "kado")
+        {
+            //親オブジェクトを取得
+            GameObject objParent = this.transform.parent.gameObject;
+            //親オブジェクトのコンポーネント(Script)を取得
+            Enemy EnemyScript = objParent.GetComponent<Enemy>();
+            EnemyScript.cross = true;
+        }
+    }
+
+}
---
+++ Assets/IchihashiFile/EnemySensor.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySensor : MonoBehaviour
+{
+    //向き
+    public const int NO = 0;
+    public const int UP = 1;
+    public const int RIGHT = 2;
+    public const int DOWN = 3;
+    public const int LEFT = 4;
+
+    //センサーの位置
+    public int Ichi = NO;
+    //親オブジェクト
+    GameObject objParent;
+    //親オブジェクトのスクリプト
+    Enemy EnemyScript;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //親オブジェクトを取得
+        objParent = this.transform.parent.gameObject;
+        //親オブジェクトのコンポーネント(Script)を取得
+        EnemyScript = objParent.GetComponent<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //当たり判定があったとき
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+
+            switch (Ichi)
+            {
+                case UP:
+                    EnemyScript.WallUp = true;
+                    break;
+                case RIGHT:
+                    EnemyScript.WallRight = true;
+                    break;
+                case DOWN:
+                    EnemyScript.WallDown = true;
+                    break;
+                case LEFT:
+                    EnemyScript.WallLeft = true;
+                    break;
+            }
+        }
+    }
+    //当たり判定があったとき
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            switch (Ichi)
+            {
+                case UP:
+                    EnemyScript.WallUp = false;
+                    break;
+                case RIGHT:
+                    EnemyScript.WallRight = false;
+                    break;
+                case DOWN:
+                    EnemyScript.WallDown = false;
+                    break;
+                case LEFT:
+                    EnemyScript.WallLeft = false;
+                    break;
+            }
+        }
+    }
+
+}
