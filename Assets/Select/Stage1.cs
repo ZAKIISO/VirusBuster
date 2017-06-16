cf0ae34478c6678d5b8547b87c28465c9715be1e
Junpei Saito <nkc.junpei.saito@gmail.com>
2017-06-16 14:00:53 +0900

    スクリプト更新

--- Assets/Select/Stage1.cs
+++ Assets/Select/Stage1.cs
@@ -8,16 +8,20 @@ public class Stage1 : MonoBehaviour
     private Fade fade;
     private float frameCnt = 0;
     private bool SceneChange = false;
+    private SpriteRenderer ThisSp;      // スプライトレンダー
 
     void Start()
     {
         SceneChange = false;
         fade = GameObject.Find("Fade").GetComponent<Fade>();
+
+        // スプライトレンダー
+        ThisSp = gameObject.GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        // クリックしたら・・・
+        // クリックDOWN
         if (Input.GetMouseButtonDown(0))
         {
             // rayの設定
@@ -30,6 +34,33 @@ public class Stage1 : MonoBehaviour
 
                 // クリックしたオブジェクトがこのオブジェクトだったら
                 if (tapObject == gameObject)
+                {
+                    // 画像小さく
+                    transform.localScale = new Vector2(0.28f, 0.28f);
+                    // 色変更
+                    ThisSp.color = new Color(0.6f, 0.6f, 0.6f);
+                }
+            }
+        }
+
+        // クリックしたら・・・
+        if (Input.GetMouseButtonUp(0))
+        {
+            // 画像大きく
+            transform.localScale = new Vector2(0.3f, 0.3f);
+            // 色変更
+            ThisSp.color = new Color(1, 1, 1);
+
+            // rayの設定
+            Vector2 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D collition2d = Physics2D.OverlapPoint(tapPoint);
+            if (collition2d)
+            {
+                // クリックしたオブジェクトを取得
+                GameObject tapObject = collition2d.transform.gameObject;
+
+                // クリックしたのがこのオブジェクトだったら
+                if (tapObject == GameObject.Find("Stage1"))// ステージ１個だから仮（本当はgameObject）
                 {
                     fade.enabled = true; // fade開始
                     SceneChange = true;  // シーンチェンジフラグ
@@ -46,7 +77,7 @@ public class Stage1 : MonoBehaviour
             if (frameCnt % 60 == 0)
             {
                 // シーン移動
-                SceneManager.LoadScene("stage");
+                SceneManager.LoadScene("test");
                 frameCnt = 0;
                 SceneChange = false;
             }
